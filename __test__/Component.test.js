6592c40e142fe3dca93ae34a7082a62654634e63
David Yan <david.yan.software@gmail.com>
2020-01-17 18:40:44 -0800

    checking component.gameObject is undefined before added

--- __test__/Component.test.js
+++ __test__/Component.test.js
@@ -5,8 +5,9 @@ describe("Component", () => {
   test("Components store a reference to their GameObject", () => {
     const gameObject = new GameObject();
     const component = new Component();
-    gameObject.addComponent(component);
+    expect(component.gameObject).not.toBeDefined();
 
+    gameObject.addComponent(component);
     expect(component.gameObject).toBe(gameObject);
   });
 });
