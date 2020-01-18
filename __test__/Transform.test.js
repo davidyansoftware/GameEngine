7940514f109dfaf144f66f8c6811ad5af4a4a5fa
David Yan <david.yan.software@gmail.com>
2020-01-17 20:41:34 -0800

    split up Transform constructor tests

--- __test__/Transform.test.js
+++ __test__/Transform.test.js
@@ -1,11 +1,13 @@
 const GameObject = require("../src/GameObject");
 
 describe("Transform", () => {
-  test("Transform defaults to (0,0)", () => {
+  test("Transform constructor defaults to (0,0)", () => {
     const defaultGameObject = new GameObject();
     expect(defaultGameObject.transform.x).toBe(0);
     expect(defaultGameObject.transform.y).toBe(0);
+  });
 
+  test("Transform constructor sets x and y coordinates", () => {
     const X_VALUE = 1;
     const Y_VALUE = 2;
     const gameObject = new GameObject(X_VALUE, Y_VALUE);
