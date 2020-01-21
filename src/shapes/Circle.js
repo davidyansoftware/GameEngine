678a85f2aca560ed61398c64d9ef0bfdbcae68e6
David Yan <david.yan.software@gmail.com>
2020-01-20 23:43:57 -0800

    refactor hit detection to handle additional Shapes

--- src/shapes/Circle.js
+++ src/shapes/Circle.js
@@ -10,19 +10,23 @@ class Circle {
   }
 
   isCollidingWith(self, hurtbox) {
+    return hurtbox.shape.isCollidingWithCircle(hurtbox, self);
+  }
+
+  isCollidingWithCircle(self, hitbox) {
     let distanceBetween = Math.sqrt(
       Math.pow(
         self.gameObject.transform.absoluteX -
-          hurtbox.gameObject.transform.absoluteX,
+          hitbox.gameObject.transform.absoluteX,
         2
       ) +
         Math.pow(
           self.gameObject.transform.absoluteY -
-            hurtbox.gameObject.transform.absoluteY,
+            hitbox.gameObject.transform.absoluteY,
           2
         )
     );
-    return distanceBetween <= self.shape.radius + hurtbox.shape.radius;
+    return distanceBetween <= self.shape.radius + hitbox.shape.radius;
   }
 }
 
