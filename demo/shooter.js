eebbdcac78da7dad4009a5cc117cf9c7d89fdf08
David Yan <david.yan.software@gmail.com>
2020-04-09 01:57:58 -0700

    adding bullets to demo

--- demo/shooter.js
+++ demo/shooter.js
@@ -4,6 +4,23 @@ const root = new DNA.GameObject();
 const camera = new DNA.Components.Camera(canvas, root);
 root.addComponent(camera);
 
+const coordinate = new DNA.Coordinate.Cartesian(0, 0);
+function createBullet(x, y, angle) {
+  const BULLET_SPEED = 100;
+
+  const bullet = new DNA.GameObject(x, y);
+
+  coordinate.magnitude = BULLET_SPEED;
+  coordinate.angle = angle;
+  const physics = new DNA.Components.Physics(coordinate.x, coordinate.y);
+  bullet.addComponent(physics);
+
+  const shape = new DNA.Components.Shape(new DNA.ShapeTypes.Circle(2));
+  bullet.addComponent(shape);
+
+  return bullet;
+}
+
 const LEFT_MOUSE_BUTTON = 0;
 
 const W_KEY_CODE = 87;
@@ -14,11 +31,15 @@ const D_KEY_CODE = 68;
 const SPEED = 500;
 
 class Player extends DNA.Component {
-  constructor(mouse, physics) {
+  constructor(root, mouse, physics) {
     super();
+
+    this.root = root;
 
     this.mouse = mouse;
     this.physics = physics;
+
+    this.mouseOffset = new DNA.Coordinate.Cartesian(0, 0);
 
     document.addEventListener("mousedown", event => {
       console.log(event);
@@ -29,6 +50,17 @@ class Player extends DNA.Component {
     const leftMouseButton = this.mouse.getButton(LEFT_MOUSE_BUTTON);
     if (leftMouseButton.pressed) {
       console.log("firing!");
+
+      this.mouseOffset.x = this.mouse.x - this.transform.x;
+      this.mouseOffset.y = this.mouse.y - this.transform.y;
+
+      const bullet = createBullet(
+        this.transform.x,
+        this.transform.x,
+        this.mouseOffset.angle
+      );
+
+      this.root.addGameObject(bullet);
     }
 
     const dx = this.mouse.x - this.transform.absoluteX;
@@ -59,7 +91,7 @@ const player = new DNA.GameObject();
 player.addComponent(new DNA.Components.Shape(new DNA.ShapeTypes.Circle(20)));
 const physics = new DNA.Components.Physics(0, 0);
 player.addComponent(physics);
-player.addComponent(new Player(mouse, physics));
+player.addComponent(new Player(root, mouse, physics));
 const gun = new DNA.GameObject(0, 20);
 gun.addComponent(new DNA.Components.Shape(new DNA.ShapeTypes.Circle(10)));
 player.addGameObject(gun);
