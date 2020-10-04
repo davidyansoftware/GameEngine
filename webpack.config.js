3086016cca394511f923c3eeb37bb225ce09c95f
David Yan <davidyansoftware@gmail.com>
2020-10-04 02:05:08 -0700

    enabling typescript

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,23 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "DNA.js",
     globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
   }
 };
