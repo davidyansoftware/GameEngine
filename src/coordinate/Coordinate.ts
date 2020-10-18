19b0b636e3c514d47d80370354f42276e375a484
David Yan <davidyansoftware@gmail.com>
2020-10-18 00:58:47 -0700

    restricting field visibility in coordinate class

--- src/coordinate/Coordinate.ts
+++ src/coordinate/Coordinate.ts
@@ -1,44 +1,29 @@
-function recalculateCartesian(coordinate: Coordinate) {
-  coordinate._x = Math.sin(coordinate._angle) * coordinate._magnitude;
-  coordinate._y = Math.cos(coordinate._angle) * coordinate._magnitude;
-  coordinate._dirtyCartesian = false;
-}
-
-//TODO can recalculate angle and magnitude seperately
-function recalculatePolar(coordinate: Coordinate) {
-  coordinate._magnitude = Math.sqrt(
-    Math.pow(coordinate._x, 2) + Math.pow(coordinate._y, 2)
-  );
-  coordinate._angle = -Math.atan2(coordinate._y, coordinate._x) + Math.PI / 2;
-  coordinate._dirtyPolar = false;
-}
-
 /**
  * Represent coordinates
  */
 export default abstract class Coordinate {
-  _x: number = 0;
-  _y: number = 0;
+  protected _x: number = 0;
+  protected _y: number = 0;
 
-  _magnitude: number = 0;
-  _angle: number = 0;
+  protected _magnitude: number = 0;
+  protected _angle: number = 0;
 
-  _dirtyCartesian: boolean = false;
-  _dirtyPolar: boolean = false;
+  protected _dirtyCartesian: boolean = false;
+  protected _dirtyPolar: boolean = false;
 
   constructor() {
   }
 
   get x(): number {
     if (this._dirtyCartesian) {
-      recalculateCartesian(this);
+      this.recalculateCartesian();
     }
 
     return this._x;
   }
   set x(x: number) {
     if (this._dirtyCartesian) {
-      recalculateCartesian(this);
+      this.recalculateCartesian();
     }
 
     this._x = x;
@@ -46,14 +31,14 @@ export default abstract class Coordinate {
   }
   get y(): number {
     if (this._dirtyCartesian) {
-      recalculateCartesian(this);
+      this.recalculateCartesian();
     }
 
     return this._y;
   }
   set y(y: number) {
     if (this._dirtyCartesian) {
-      recalculateCartesian(this);
+      this.recalculateCartesian();
     }
 
     this._y = y;
@@ -62,14 +47,14 @@ export default abstract class Coordinate {
 
   get magnitude(): number {
     if (this._dirtyPolar) {
-      recalculatePolar(this);
+      this.recalculatePolar();
     }
 
     return this._magnitude;
   }
   set magnitude(magnitude: number) {
     if (this._dirtyPolar) {
-      recalculatePolar(this);
+      this.recalculatePolar();
     }
 
     this._magnitude = magnitude;
@@ -77,17 +62,32 @@ export default abstract class Coordinate {
   }
   get angle(): number {
     if (this._dirtyPolar) {
-      recalculatePolar(this);
+      this.recalculatePolar();
     }
 
     return this._angle;
   }
   set angle(angle: number) {
     if (this._dirtyPolar) {
-      recalculatePolar(this);
+      this.recalculatePolar();
     }
 
     this._angle = angle;
     this._dirtyCartesian = true;
+  }
+
+  private recalculateCartesian() {
+    this._x = Math.sin(this._angle) * this._magnitude;
+    this._y = Math.cos(this._angle) * this._magnitude;
+    this._dirtyCartesian = false;
+  }
+  
+  //TODO can recalculate angle and magnitude seperately
+  private recalculatePolar() {
+    this._magnitude = Math.sqrt(
+      Math.pow(this._x, 2) + Math.pow(this._y, 2)
+    );
+    this._angle = -Math.atan2(this._y, this._x) + Math.PI / 2;
+    this._dirtyPolar = false;
   }
 }
