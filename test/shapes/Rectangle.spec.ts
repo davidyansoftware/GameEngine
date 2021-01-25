9e4162229b9f1bd3fccd2eb15258b21beec47ec1
David Yan <davidyansoftware@gmail.com>
2021-01-24 22:23:32 -0800

    migrating rectangle tests

--- test/shapes/Rectangle.spec.ts
+++ test/shapes/Rectangle.spec.ts
@@ -1,17 +1,19 @@
+import * as assert from "assert";
+import * as sinon from "sinon";
 import Rectangle from "../../src/shapes/Rectangle";
 import Canvas from "canvas";
 
 describe("Rectangle render", () => {
-    test("render renders a rectangle", () => {
+    it("renders a rectangle", () => {
         const canvas = Canvas.createCanvas(100, 100);
         const ctx = canvas.getContext("2d");
-        jest.spyOn(ctx, "strokeRect");
+        const ctxSpy = sinon.spy(ctx, "strokeRect");
 
         const WIDTH = 5;
         const HEIGHT = 10;
         const rectangle = new Rectangle(WIDTH, HEIGHT);
         rectangle.render(ctx);
-        expect(ctx.strokeRect).toHaveBeenCalledTimes(1);
-        expect(ctx.strokeRect).toHaveBeenLastCalledWith(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT);
+        
+        assert.ok(ctxSpy.calledOnceWith(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT));
     });
 });
