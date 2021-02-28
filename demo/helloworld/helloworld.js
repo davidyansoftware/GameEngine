6129b0daa7ce288e92f4b63d8388ae4de7e3f242
David Yan <davidyansoftware@gmail.com>
2021-02-27 17:25:23 -0800

    adjusted follow component in demo

--- demo/helloworld/helloworld.js
+++ demo/helloworld/helloworld.js
@@ -1,16 +1,20 @@
 const canvas = document.getElementById("canvas");
 
-const root = new DNA.GameObject({shape: new DNA.Shapes.Circle(100)});
+const root = new DNA.GameObject();
 
 const cameraGameObject = new DNA.GameObject();
 const camera = new DNA.Components.Camera(canvas, root);
 cameraGameObject.addComponent(camera);
 root.addGameObject(cameraGameObject);
 
+const circleGameObject = new DNA.GameObject({shape: new DNA.Shapes.Circle(100)});
+circleGameObject.addComponent(new DNA.Components.Renderer());
 const textGameObject = new DNA.GameObject();
 const text = new DNA.Components.Text("Hello World!");
 textGameObject.addComponent(text);
-root.addGameObject(textGameObject);
+circleGameObject.addGameObject(textGameObject);
+
+root.addGameObject(circleGameObject);
 
 root.addComponent(new DNA.Components.Renderer());
 
@@ -22,16 +26,16 @@ class Follow extends DNA.Component {
   }
 
   update() {
-    const dx = this.mouse.x;
-    const dy = this.mouse.y;
-    this.transform.position.x += dx / 100;
-    this.transform.position.y += dy / 100;
+    const dx = this.mouse.x - this.transform.position.x;
+    const dy = this.mouse.y - this.transform.position.y;
+    this.transform.position.x += dx / 10;
+    this.transform.position.y += dy / 10;
   }
 }
 
 const mouse = new DNA.Input.Mouse(document, camera);
 
 const follow = new Follow(mouse);
-cameraGameObject.addComponent(follow);
+circleGameObject.addComponent(follow);
 
 new DNA.GameLoop(root);
